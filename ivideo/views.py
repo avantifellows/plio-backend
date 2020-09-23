27d0ea16056cd3d6040361a404e74b09e099f7e8
Pritam Sukumar <pritamps@gmail.com>
2020-09-23 10:07:51 +0530

    Checking for existence of object dID

--- ivideo/views.py
+++ ivideo/views.py
@@ -1,3 +1,4 @@
+import ivideo
 from django.http import request
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
@@ -9,7 +10,7 @@ from utils.avanti_s3 import get_all_ivideo_objects
 
 def index(request):
     all_ivideos = get_all_ivideo_objects()
-
+    all_ivideos = [ivideo for ivideo in all_ivideos if "object_id" in ivideo]
     context = {
         "all_ivideos": all_ivideos
     }

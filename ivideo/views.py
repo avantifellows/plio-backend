f3ee3b132fdbc22ba71dfc6ac2bebdb481fad885
Aman <aman@wadhwaniai.org>
2020-10-06 11:54:29 +0530

    ENH: moved aws auth to django

--- ivideo/views.py
+++ ivideo/views.py
@@ -1,3 +1,4 @@
+from os.path import join
 from django.http import response
 import ivideo
 from django.http import HttpResponseBadRequest, request
@@ -9,7 +10,33 @@ from rest_framework.decorators import api_view
 
 import json
 
-from utils.avanti_s3 import get_all_ivideo_objects, get_object
+from utils.avanti_s3 import get_all_ivideo_objects, get_object, get_resource
+
+
+@api_view(['POST'])
+def update_response(request):
+    print(request.data)
+
+    meta_data = request.data['meta']
+    response = request.data['response']
+
+    # authenticate
+    s3 = get_resource()
+
+    # define bucket
+    bucket = 'avanti-fellows'
+
+    # directory where responses are saved
+    save_dir = 'answers'
+
+    # define the path where the response is saved
+    file_name = f"{meta_data['object_id']}_{meta_data['student_id']}.json"
+    file_path = join(save_dir, file_name)
+
+    s3.Object(bucket, file_path).put(Body=json.dumps(response))
+    return JsonResponse({
+        'path': f"http://avanti-fellows.s3.ap-south-1.amazonaws.com/{file_path}"
+    }, status=200)
 
 
 @api_view(['GET'])
@@ -20,18 +47,20 @@ def get_ivideos_list(request):
     }
     return JsonResponse(response)
 
+
 @api_view(['GET'])
 def get_ivideo(request):
     ivideo_id = request.GET.get('ivideo_id', '')
-    if (ivideo_id ==''):
-        return JsonResponse({"Error": "No Video specified"}, status=HttpResponseBadRequest)
-    
+    if not ivideo_id:
+        return JsonResponse(
+            {"Error": "No Video specified"}, status=HttpResponseBadRequest)
+
     data = get_object(f'videos/{ivideo_id}.json')
-    if data == None:
+    if data is None:
         return HttpResponseNotFound('<h1>Video not found</h1>')
-    
+
     jsondata = json.loads(data)
-    
+
     questions = []
     times = []
     options = []
@@ -52,6 +81,7 @@ def get_ivideo(request):
 
     return JsonResponse(response, status=200)
 
+
 def index(request):
     all_ivideos = get_all_ivideo_objects()
     all_ivideos = [ivideo for ivideo in all_ivideos if "object_id" in ivideo]
@@ -60,5 +90,3 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
-    
-
--- utils/avanti_s3.py
+++ utils/avanti_s3.py
@@ -3,6 +3,8 @@ from botocore.exceptions import ClientError
 import json
 
 # NOTE: public authentication details
+
+
 def get_resource(
         service_name='s3', region_name='ap-south-1',
         aws_access_key_id='AKIAQHKYBVIIKK3LUGF6',
@@ -16,25 +18,26 @@ def get_resource(
         aws_secret_access_key=aws_secret_access_key
     )
 
+
 def get_object(key, bucket="avanti-fellows"):
     s3 = get_resource()
     try:
         obj = s3.Object(bucket, key)
-        return obj.get()['Body'].read().decode('utf-8') 
+        return obj.get()['Body'].read().decode('utf-8')
     except ClientError as e:
         if e.response['Error']['Code'] == 'NoSuchKey':
             return None
     return None
 
+
 def get_all_ivideo_objects(bucket='avanti-fellows', extensions=['json']):
     s3 = get_resource()
     s3_bucket = s3.Bucket(bucket)
 
-    #get all files information from buket
+    # get all files information from buket
     files = s3_bucket.objects.filter(Prefix='videos/', Delimiter='/')
     # create empty list for final information
     matching_files = []
-    
 
     # Iterate throgh 'files', convert to dict. and add extension key.
     for file in files:
@@ -42,12 +45,13 @@ def get_all_ivideo_objects(bucket='avanti-fellows', extensions=['json']):
         name = file.key.split('/')[-1].split('.')[0]
         if ext in extensions:
             try:
-                json_content = json.loads( s3.Object(bucket, file.key).get()['Body'].read().decode('utf-8') )
-                matching_files.append(dict({"object_id": name, "details": json_content }))
-            except:
+                json_content = json.loads(
+                    s3.Object(bucket, file.key).get()['Body'].read().decode(
+                        'utf-8'))
+                matching_files.append(dict({
+                    "object_id": name, "details": json_content
+                }))
+            except e:
                 pass
-            
-
 
     return matching_files
-        

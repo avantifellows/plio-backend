c17bbd955a1f605ee6f5bbf71a38fd09b1b3b4f6
Pritam Sukumar <pritamps@gmail.com>
2020-10-08 13:27:17 +0530

    Added Created Date

--- utils/avanti_s3.py
+++ utils/avanti_s3.py
@@ -6,6 +6,7 @@ import json
 
 import urllib.request
 import urllib
+import datetime
 # NOTE: public authentication details
 
 def get_video_tile(videoId):
@@ -80,11 +81,13 @@ def get_all_ivideo_objects(bucket='avanti-fellows', extensions=['json']):
 
     # get all files information from buket
     files = s3_bucket.objects.filter(Prefix='videos/', Delimiter='/')
+    print(files)
     # create empty list for final information
     matching_files = []
 
     # Iterate throgh 'files', convert to dict. and add extension key.
     for file in files:
+
         ext = file.key.split('.')[-1]
         name = file.key.split('/')[-1].split('.')[0]
         if ext in extensions:
@@ -93,8 +96,9 @@ def get_all_ivideo_objects(bucket='avanti-fellows', extensions=['json']):
                     s3.Object(bucket, file.key).get()['Body'].read().decode(
                         'utf-8'))
                 video_title = get_video_tile(json_content["video_id"])
+                date = datetime.datetime.strftime(file.last_modified, "%Y-%m-%d")
                 matching_files.append(dict({
-                    "object_id": name, "details": json_content, "title": video_title
+                    "object_id": name, "details": json_content, "title": video_title, "created": date
                 }))
             except Exception as e:
                 print(e)

edf6824db9808e6be9ff144fe0443ccb5dd374c7
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-02-07 13:55:23 +0530

    FIX: changed url linked to functions to be consistent

--- components/urls.py
+++ components/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get_default_config', views._get_default_component_config),
-    path('get_features', views._get_component_features),
+    path('get_default_config', views._get_default_config),
+    path('get_features', views._get_features),
 ]

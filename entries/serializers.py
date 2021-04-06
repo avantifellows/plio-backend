f4002335d832acc6aa6ef5292f0ae60b0b022d7a
Aman Dalmia <amandalmia18@gmail.com>
2021-04-06 14:38:13 +0530

    FIX: added ordering for sessions + autoload session data on session creation

--- entries/serializers.py
+++ entries/serializers.py
@@ -32,6 +32,18 @@ class SessionSerializer(serializers.ModelSerializer):
         """
         Create and return a new `Session` instance, given the validated data.
         """
+        # fetch all past sessions for this user-plio combination
+        past_sessions = Session.objects.filter(
+            plio__id=validated_data["plio"].id
+        ).filter(user__id=validated_data["user"].id)
+        if past_sessions:
+            # add values for missing keys from the most recent session
+            last_session_data = SessionSerializer(past_sessions[0]).data
+            keys_to_check = ["retention", "has_video_played", "experiment"]
+            for key in keys_to_check:
+                if key not in validated_data:
+                    validated_data[key] = last_session_data[key]
+
         return Session.objects.create(**validated_data)
 
     def to_representation(self, instance):

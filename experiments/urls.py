b0eec94de863440cd5b6505c2fa2560df57deca1
Aman Dalmia <amandalmia18@gmail.com>
2021-04-06 18:59:17 +0530

    FIX: removed useless stuff and filled in missing details for experiments

--- experiments/urls.py
+++ experiments/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("get_assignment", views.get_assignment),
-    path("get_df", views.get_df),
 ]
--- plio/views.py
+++ plio/views.py
@@ -278,28 +278,6 @@ def get_plio_config(plio_id: str):
     return plio_config.json()["plio_config"]
 
 
-def index(request: Request):
-    """Renders home page for backend"""
-    plios = get_all_plios()
-
-    # if the returned object is not dict, it will be some variant
-    # of HttpResponseNotFound, returning it if that's the case
-    if not isinstance(plios, list):
-        return plios
-
-    return render(request, "index.html", {"all_plios": plios})
-
-
-def redirect_home(request: Request):
-    """Redirect to frontend home"""
-    return redirect(FRONTEND_URL, permanent=True)
-
-
-def redirect_plio(request: Request, plio_id: str):
-    """Redirect to frontend plio page"""
-    return redirect(f"{FRONTEND_URL}/#/play/{plio_id}", permanent=True)
-
-
 class VideoViewSet(viewsets.ModelViewSet):
     """
     Video ViewSet description

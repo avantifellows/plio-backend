1c608369894dd7ac4a0278180e1a0ed1571a5e64
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-04-08 13:53:45 +0530

    pre commit errors

--- plio/migrations/0015_auto_20210408_0638.py
+++ plio/migrations/0015_auto_20210408_0638.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('plio', '0014_auto_20210407_0553'),
+        ("plio", "0014_auto_20210407_0553"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='item',
-            name='time',
+            model_name="item",
+            name="time",
             field=models.FloatField(),
         ),
         migrations.AlterField(
-            model_name='video',
-            name='duration',
+            model_name="video",
+            name="duration",
             field=models.FloatField(null=True),
         ),
     ]

1748eae128d1e426fbf1186f20700c3bdc33298c
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-04-13 14:39:28 +0530

    review feedbacks

--- organizations/middleware.py
+++ organizations/middleware.py
@@ -6,21 +6,30 @@ from django_tenants.utils import get_public_schema_name, get_tenant_model
 
 class OrganizationTenantMiddleware(TenantMainMiddleware):
     """
-    Determines tenant by the value of the ``ORGANIZATION`` HTTP header.
+    Sets connection to either public or tenant schema based on `ORGANIZATION` HTTP header value.
     """
 
-    def get_tenant(self, tenant_model, hostname, request):
+    def get_tenant(self, tenant_model, request):
+        """
+        Determines tenant by the value of the `ORGANIZATION` HTTP header.
+        """
         organization_shortcode = request.META.get(
             "HTTP_ORGANIZATION", get_public_schema_name()
         )
         return tenant_model.objects.filter(shortcode=organization_shortcode).first()
 
     def process_request(self, request):
+        """
+        Switches connection to tenant schema if valid tenant. Otherwise keeps the connection with public schema.
+        """
         # Connection needs first to be at the public schema, as this is where the tenant metadata is stored.
         connection.set_schema_to_public()
-        hostname = self.hostname_from_request(request)
 
+        # retrieve tenant model configured in settings.py
         tenant_model = get_tenant_model()
-        tenant = self.get_tenant(tenant_model, hostname, request)
+
+        # get the right tenant object based on request
+        tenant = self.get_tenant(tenant_model, request)
         if tenant:
+            # set connection to tenant's schema
             connection.set_tenant(tenant)

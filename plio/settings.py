21736f6a9985ab32b1db25dfe8b99ec5f7723152
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-04-19 17:14:00 +0530

    dockerfile, docker-compose and zappa settings removed

--- plio/settings.py
+++ plio/settings.py
@@ -287,12 +287,12 @@ GET_CMS_PROBLEM_URL = "/problems"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django_tenants.postgresql_backend",
-        "NAME": os.environ["DATABASE_NAME"],
-        "USER": os.environ["DATABASE_USER"],
-        "PASSWORD": os.environ["DATABASE_PASSWORD"],
-        "HOST": os.environ["DATABASE_HOST"],
-        "PORT": int(os.environ["DATABASE_PORT"]),
+        "ENGINE": os.environ["DB_ENGINE"],
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": int(os.environ["DB_PORT"]),
     }
 }
 

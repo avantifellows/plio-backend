044ce4a9f63aaabb88610bab99cd55de9e5bb133
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-04-20 15:14:23 +0530

    fixture command added before server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-# start server
-python manage.py migrate && python manage.py runserver 0.0.0.0:${APP_PORT}
+# run migrations
+python manage.py migrate
 
 # create default tenant
 python manage.py loaddata organizations/fixtures/default_tenant.yaml
 
+# start the server
+python manage.py runserver 0.0.0.0:${APP_PORT}
 
 exec "$@"

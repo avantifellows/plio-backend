0b5b1180feca6d28c0d534d288f5681bd167ad6d
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-04-20 18:11:02 +0530

    review feedback

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 python manage.py migrate
 
 if [ ${APP_ENV} = "local" ]; then
+    # create default tenant
     python manage.py loaddata organizations/fixtures/default_tenant.yaml
 fi
-# create default tenant
 
 # start the server
 python manage.py runserver 0.0.0.0:${APP_PORT}

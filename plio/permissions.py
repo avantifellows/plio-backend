78247e04e7bf7d395ee8e41000d3a005847da22c
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-05-25 21:13:54 +0530

    checking for user org

--- plio/permissions.py
+++ plio/permissions.py
@@ -1,4 +1,7 @@
 from rest_framework import permissions
+from organizations.middleware import OrganizationTenantMiddleware
+from plio.settings import DEFAULT_TENANT_SHORTCODE
+from users.models import OrganizationUser
 
 
 class PlioPermission(permissions.BasePermission):
@@ -10,4 +13,23 @@ class PlioPermission(permissions.BasePermission):
         return True
 
     def has_object_permission(self, request, view, obj):
-        return request.user.is_superuser or request.user == obj.created_by
+        print("inside object permission")
+        if request.user.is_superuser:
+            return True
+
+        organization_shortcode = (
+            OrganizationTenantMiddleware.get_organization_shortcode(request)
+        )
+        if organization_shortcode == DEFAULT_TENANT_SHORTCODE:
+            return request.user == obj.created_by
+
+        user_belongs_to_organization = OrganizationUser.objects.filter(
+            organization__shortcode=organization_shortcode,
+            user=request.user.id,
+        ).exists()
+
+        if user_belongs_to_organization and obj.is_public:
+            return True
+
+        # user doesn't belong to organization or plio isn't public
+        return request.user == obj.created_by

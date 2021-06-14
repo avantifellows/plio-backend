d5c038f88ed6cb00b9dc7485a704c6f702ad5078
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-06-14 23:10:11 +0530

    FIX: pre commit errors

--- plio/migrations/0022_image_alt_text.py
+++ plio/migrations/0022_image_alt_text.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('plio', '0021_auto_20210613_1711'),
+        ("plio", "0021_auto_20210613_1711"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='image',
-            name='alt_text',
-            field=models.CharField(default='Image', max_length=255),
+            model_name="image",
+            name="alt_text",
+            field=models.CharField(default="Image", max_length=255),
         ),
     ]

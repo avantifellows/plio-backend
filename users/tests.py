80710b0b934f2466022b713e1d8841a8384d3f8e
Aman Dalmia <amandalmia18@gmail.com>
2021-06-19 10:15:53 +0530

    NEW: user config test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework import status
 from django.urls import reverse
 from users.models import OneTimePassword, User, Role
@@ -12,36 +13,35 @@ class OtpAuthTestCase(BaseTestCase):
         super().setUp()
         # unset client credentials token so that the subsequent API calls goes as guest
         self.client.credentials()
-        self.user_mobile = "+919876543210"
 
     def test_guest_can_request_for_otp(self):
         response = self.client.post(
-            reverse("request-otp"), {"mobile": self.user_mobile}
+            reverse("request-otp"), {"mobile": self.user.mobile}
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        otp_exists = OneTimePassword.objects.filter(mobile=self.user_mobile).exists()
+        otp_exists = OneTimePassword.objects.filter(mobile=self.user.mobile).exists()
         self.assertTrue(otp_exists)
 
     def test_invalid_otp_should_fail(self):
         # request otp
-        self.client.post(reverse("request-otp"), {"mobile": self.user_mobile})
+        self.client.post(reverse("request-otp"), {"mobile": self.user.mobile})
 
         # invalid otp
         otp = "000000"
         response = self.client.post(
-            reverse("verify-otp"), {"mobile": self.user_mobile, "otp": otp}
+            reverse("verify-otp"), {"mobile": self.user.mobile, "otp": otp}
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_valid_otp_should_pass(self):
         # request otp
-        self.client.post(reverse("request-otp"), {"mobile": self.user_mobile})
+        self.client.post(reverse("request-otp"), {"mobile": self.user.mobile})
 
         # verify valid otp
-        otp = OneTimePassword.objects.filter(mobile=self.user_mobile).first()
+        otp = OneTimePassword.objects.filter(mobile=self.user.mobile).first()
         response = self.client.post(
-            reverse("verify-otp"), {"mobile": self.user_mobile, "otp": otp.otp}
+            reverse("verify-otp"), {"mobile": self.user.mobile, "otp": otp.otp}
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -58,6 +58,59 @@ class OtpAuthTestCase(BaseTestCase):
             reverse("verify-otp"), {"mobile": new_user_mobile, "otp": otp.otp}
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class UserTestCase(BaseTestCase):
+    def test_get_config(self):
+        config = {"test": True}
+        # set config
+        self.user.config = config
+        self.user.save()
+
+        # get config
+        response = self.client.get(f"/api/v1/users/{self.user.id}/config/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), config)
+
+    def test_update_config_no_config_provided(self):
+        # update config
+        response = self.client.patch(f"/api/v1/users/{self.user.id}/config/", {})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_config_extra_params(self):
+        # update config
+        response = self.client.patch(
+            f"/api/v1/users/{self.user.id}/config/",
+            {"config": {}, "extra_param": True},
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            json.loads(response.content)["detail"],
+            "extra keys apart from config are not allowed",
+        )
+
+    def test_update_config(self):
+        # update config
+        config = {"test": True}
+        response = self.client.patch(
+            f"/api/v1/users/{self.user.id}/config/",
+            {"config": config},
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # get the config to see if it is updated
+        response = self.client.get(f"/api/v1/users/{self.user.id}/config/")
+
+        self.assertEqual(
+            response.json(),
+            config,
+        )
+
+    # one user should not be able to get config of another user
+    # one user should not be able to update config of another user
 
 
 class UserMetaTestCase(BaseTestCase):

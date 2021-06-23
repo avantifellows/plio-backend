542688af89af24a00c8ccc920867c3e4d18918b1
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-06-23 18:21:43 +0530

    FIX: pre-commit error

--- users/migrations/0016_merge_20210623_1241.py
+++ users/migrations/0016_merge_20210623_1241.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0015_auto_20210623_0700'),
-        ('users', '0015_auto_20210623_0822'),
+        ("users", "0015_auto_20210623_0700"),
+        ("users", "0015_auto_20210623_0822"),
     ]
 
-    operations = [
-    ]
+    operations = []

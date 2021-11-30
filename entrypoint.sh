8d044611e104a8c710ca386ab32984890c44182c
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-11-30 23:40:37 +0530

    commands rename

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,13 @@ python manage.py makemigrations
 python manage.py migrate
 
 # create default tenant
-python manage.py pliotenant
+python manage.py createtenant
 
 # create default superuser
-python manage.py pliosuperuser
+python manage.py createsuperuser
 
 # create default OAuth2 API client credentials
-python manage.py pliooauth2application
+python manage.py createoauth2application
 
 # start the server
 python manage.py runserver 0.0.0.0:${APP_PORT}

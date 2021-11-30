4b58ea4dd409b116212224797fbddf9171bf1303
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-11-30 23:43:41 +0530

    review feedbacks

--- plio/management/commands/createoauth2application.py
+++ plio/management/commands/createoauth2application.py
@@ -10,29 +10,34 @@ class Command(BaseCommand):
         default_oauth2_client_setup = os.environ.get(
             "DEFAULT_OAUTH2_CLIENT_SETUP", False
         )
+        if not default_oauth2_client_setup:
+            print("Default OAuth2 client setup is disabled. Skipping.")
+            return
+
         default_oauth2_client_id = os.environ.get("DEFAULT_OAUTH2_CLIENT_ID")
         default_oauth2_client_secret = os.environ.get("DEFAULT_OAUTH2_CLIENT_SECRET")
-        if default_oauth2_client_setup:
-            if default_oauth2_client_id and default_oauth2_client_secret:
-                print(
-                    "Default OAuth2 client id and secret provided. Creating default application."
-                )
-                application = Application.objects.filter(
-                    client_id=default_oauth2_client_id
-                ).first()
-                if application:
-                    print("Default OAuth2 client id already exists. Skipping.")
-                else:
-                    Application.objects.create(
-                        name="default",
-                        client_id=default_oauth2_client_id,
-                        client_secret=default_oauth2_client_secret,
-                        redirect_uris="",
-                        client_type=Application.CLIENT_CONFIDENTIAL,
-                        authorization_grant_type=Application.GRANT_PASSWORD,
-                    )
-                print("Created default OAuth2 client id and secret!")
-            else:
-                print("No default OAuth2 client id and secret provided. Skipping.")
-        else:
-            print("Default OAuth2 client setup is disabled. Skipping.")
+
+        if not default_oauth2_client_id or not default_oauth2_client_secret:
+            print("No default OAuth2 client id and secret provided. Skipping.")
+            return
+
+        print(
+            "Default OAuth2 client id and secret provided. Creating default application."
+        )
+        application = Application.objects.filter(
+            client_id=default_oauth2_client_id
+        ).first()
+
+        if application:
+            print("Default OAuth2 client id already exists. Skipping.")
+            return
+
+        Application.objects.create(
+            name="default",
+            client_id=default_oauth2_client_id,
+            client_secret=default_oauth2_client_secret,
+            redirect_uris="",
+            client_type=Application.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Application.GRANT_PASSWORD,
+        )
+        print("Created default OAuth2 client id and secret!")

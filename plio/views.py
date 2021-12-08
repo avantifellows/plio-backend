0bf1a4389f81ed440d3b0f00cbb110828c332279
Aman Dalmia <amandalmia18@gmail.com>
2021-12-08 13:26:36 +0530

    FIX: send raw total count of plios in paginated response

--- plio/views.py
+++ plio/views.py
@@ -54,7 +54,7 @@ class StandardResultsSetPagination(PageNumberPagination):
     # number of results in a page
     page_size = 5
 
-    def get_paginated_response(self, data):
+    def get_paginated_response(self, params):
         # a paginated response will follow this structure
         return Response(
             {
@@ -62,7 +62,8 @@ class StandardResultsSetPagination(PageNumberPagination):
                 "page_size": self.get_page_size(self.request),
                 "next": self.get_next_link(),
                 "previous": self.get_previous_link(),
-                "results": data,
+                "results": params["data"],
+                "raw_count": params["raw_count"],
             }
         )
 
@@ -128,7 +129,9 @@ class PlioViewSet(viewsets.ModelViewSet):
     @action(detail=False, permission_classes=[IsAuthenticated])
     def list_uuid(self, request):
         """Retrieves a list of UUIDs for all the plios"""
-        queryset = self.filter_queryset(self.get_queryset())
+        queryset = self.get_queryset()
+        num_plios = queryset.count()
+        queryset = self.filter_queryset(queryset)
 
         organization_shortcode = (
             OrganizationTenantMiddleware.get_organization_shortcode(self.request)
@@ -153,7 +156,7 @@ class PlioViewSet(viewsets.ModelViewSet):
         page = self.paginate_queryset(uuid_list)
 
         if page is not None:
-            return self.get_paginated_response(page)
+            return self.get_paginated_response({"data": page, "raw_count": num_plios})
 
         # return an empty response in the paginated format if pagination fails
         return Response(

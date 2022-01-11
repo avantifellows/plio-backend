dc9798b559581d7633506f806acd13ed50f7d99e
Aman Dalmia <amandalmia18@gmail.com>
2022-01-11 16:55:48 +0530

    NEW: added tests for plio + fixed tests for video

--- plio/tests.py
+++ plio/tests.py
@@ -541,6 +541,57 @@ class PlioTestCase(BaseTestCase):
         self.assertEqual(len(cache.keys(cache_key_name)), 1)
         self.assertEqual(cache.get(cache_key_name)["name"], new_name)
 
+    def test_copying_without_workspace_fails(self):
+        response = self.client.post(f"/api/v1/plios/{self.plio_1.id}/copy/")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_copying_without_video_fails(self):
+        response = self.client.post(f"/api/v1/plios/{self.plio_1.id}/copy/")
+        self.assertEqual(
+            response.status_code, status.HTTP_400_BAD_REQUEST, {"workspace": "abcd"}
+        )
+
+    def test_copying_to_non_existing_workspace_fails(self):
+        response = self.client.post(
+            f"/api/v1/plios/{self.plio_1.id}/copy/", {"workspace": "abcd", "video": 1}
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_copying_to_workspace(self):
+        # create a video instance assuming that video has been copied
+        connection.set_schema(self.organization.schema_name)
+
+        video = Video.objects.create(
+            title="Video 1", url="https://www.youtube.com/watch?v=vnISjBbrNUN"
+        )
+
+        # set db connection back to public (default) schema
+        connection.set_schema_to_public()
+
+        response = self.client.post(
+            f"/api/v1/plios/{self.plio_1.uuid}/copy/",
+            {"workspace": self.organization.shortcode, "video": video.id},
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_plio_uuid = response.data["uuid"]
+
+        # the plio in the personal workspace should keep existing
+        response = self.client.get(f"/api/v1/plios/{self.plio_1.uuid}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # check that the plio instance is actually created in the given workspace
+        connection.set_schema(self.organization.schema_name)
+
+        response = self.client.get(
+            f"/api/v1/plios/{new_plio_uuid}/",
+            HTTP_ORGANIZATION=self.organization.shortcode,
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["id"], video.id)
+
+        # set db connection back to public (default) schema
+        connection.set_schema_to_public()
+
 
 class PlioDownloadTestCase(BaseTestCase):
     def setUp(self):
@@ -680,34 +731,41 @@ class VideoTestCase(BaseTestCase):
             cache.get(cache_key_name)["video"]["title"], new_title_for_video
         )
 
-    def test_copying_video_without_workspace_fails(self):
+    def test_copying_without_workspace_fails(self):
         response = self.client.post(f"/api/v1/videos/{self.video_1.id}/copy/")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_copying_video_to_non_existing_workspace_fails(self):
+    def test_copying_to_non_existing_workspace_fails(self):
         response = self.client.post(
             f"/api/v1/videos/{self.video_1.id}/copy/", {"workspace": "abcd"}
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_copying_video_to_workspace(self):
+    def test_copying_to_workspace(self):
+        video = Video.objects.create(
+            title="Video 2", url="https://www.youtube.com/watch?v=vnISjBbrMUM"
+        )
         response = self.client.post(
-            f"/api/v1/videos/{self.video_1.id}/copy/",
+            f"/api/v1/videos/{video.id}/copy/",
             {"workspace": self.organization.shortcode},
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         new_video_id = response.data["id"]
 
         # the video in the personal workspace should keep existing
-        response = self.client.get(f"/api/v1/videos/{self.video_1.id}/")
+        response = self.client.get(f"/api/v1/videos/{video.id}/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # check that the video instance is actually creating in the given workspace
         connection.set_schema(self.organization.schema_name)
 
-        response = self.client.get(f"/api/v1/videos/{new_video_id}/")
+        response = self.client.get(
+            f"/api/v1/videos/{new_video_id}/",
+            HTTP_ORGANIZATION=self.organization.shortcode,
+        )
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["title"], video.title)
 
         # set db connection back to public (default) schema
         connection.set_schema_to_public()

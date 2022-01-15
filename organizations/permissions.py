fb56f644724b13c846da6c462adb6b34a2a8e2c6
Deepansh Mathur <deepansh.mathur96@gmail.com>
2022-01-15 15:15:55 +0530

    tests fix

--- organizations/permissions.py
+++ organizations/permissions.py
@@ -8,12 +8,18 @@ class OrganizationPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         """View-level permissions for organization. This determines whether the request can access organization instances or not."""
-        return request.user.is_superuser or request.user.is_org_admin(
-            organization_id=int(view.kwargs["pk"])
-        )
+        if view.action == "update":
+            org_to_update = int(view.kwargs["pk"])
+            return request.user.is_superuser or request.user.is_org_admin(
+                organization_id=org_to_update
+            )
+        return request.user.is_superuser
 
     def has_object_permission(self, request, view, obj):
         """Object-level permissions for an organization. This determines whether the request can access an organization instance or not."""
-        return request.user.is_superuser or request.user.is_org_admin(
-            organization_id=int(view.kwargs["pk"])
-        )
+        if view.action == "update":
+            org_to_update = int(view.kwargs["pk"])
+            return request.user.is_superuser or request.user.is_org_admin(
+                organization_id=org_to_update
+            )
+        return request.user.is_superuser

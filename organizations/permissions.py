406068714fbe0ae9b9b1c224524cc680a679f38b
Deepansh Mathur <deepansh.mathur96@gmail.com>
2022-01-21 14:09:01 +0530

    permissions bug fixed

--- organizations/permissions.py
+++ organizations/permissions.py
@@ -8,7 +8,7 @@ class OrganizationPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         """View-level permissions for organization. This determines whether the request can access organization instances or not."""
-        if view.action == "update":
+        if view.action in ["setting"]:
             org_to_update = int(view.kwargs["pk"])
             return request.user.is_superuser or request.user.is_org_admin(
                 organization_id=org_to_update
@@ -17,7 +17,7 @@ class OrganizationPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """Object-level permissions for an organization. This determines whether the request can access an organization instance or not."""
-        if view.action == "update":
+        if view.action in ["setting"]:
             org_to_update = int(view.kwargs["pk"])
             return request.user.is_superuser or request.user.is_org_admin(
                 organization_id=org_to_update

7a6a5b1575a00a0ddcc193be3442cfa0ca9d9d15
Aman Dalmia <amandalmia18@gmail.com>
2022-02-18 13:57:33 +0530

    FIX: tests

--- plio/tests.py
+++ plio/tests.py
@@ -265,7 +265,6 @@ class PlioTestCase(BaseTestCase):
 
         for index, _ in enumerate(expected_results):
             expected_results[index]["unique_viewers"] = 0
-            expected_results[index]["items"] = []
             expected_results[index]["video_url"] = self.video.url
 
         self.assertEqual(

e7426a8e09c000a0add7767ea3c3a3eb2dcf3c33
Saloni Choudhary <73693628+Salonichoudhary07@users.noreply.github.com>
2022-02-25 11:05:14 +0000

    code updated

--- plio/views.py
+++ plio/views.py
@@ -425,9 +425,9 @@ class PlioViewSet(viewsets.ModelViewSet):
                 )
 
         # question-based metrics
-        import ipdb
+        # import ipdb
 
-        ipdb.set_trace()
+        # ipdb.set_trace()
         questions = Question.objects.filter(item__plio=plio.id)
 
         # if the plio does not have any questions, these metrics are not applicable
@@ -459,7 +459,7 @@ class PlioViewSet(viewsets.ModelViewSet):
             )
 
             # retain only the responses to items which are questions
-            dff = df[df["survey"] == "TRUE"].reset_index(drop=True)
+            dff = df[df["survey"] == False].reset_index(drop=True)
             question_df = dff[dff["item_type"] == "question"].reset_index(drop=True)
             num_questions = len(questions)
 
@@ -478,11 +478,6 @@ class PlioViewSet(viewsets.ModelViewSet):
             for group in user_grouping.groups:
                 # get the responses for a given user
                 group_df = user_grouping.get_group(group)
-
-                # sanity check
-                assert num_questions == len(
-                    group_df
-                ), "Inconsistency in the number of questions"
 
                 num_answered = sum(
                     group_df["answer"].apply(lambda value: value is not None)

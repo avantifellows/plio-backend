42512268c126a89697de3c3d6c6c7b10dc19d61f
amit-dhidhi-dev <amitdhidhi778899@gmail.com>
2025-04-18 00:08:24 +0530

    fix #236 - implement ASGI deployment for plio

--- plio/asgi.py
+++ plio/asgi.py
@@ -1,13 +1,19 @@
 # Details about what this is and why this file is needed -
 # https://channels.readthedocs.io/en/latest/asgi.html#
 import os
+import django
+
+# Set environment before anything else
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plio.settings")
+
+# Initialize Django before importing anything from it
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import plio.urls
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plio.settings")
 
 application = ProtocolTypeRouter(
     {
@@ -17,3 +23,8 @@ application = ProtocolTypeRouter(
         "websocket": AuthMiddlewareStack(URLRouter(plio.urls.websocket_urlpatterns)),
     }
 )
+
+# asgi.py is used to run the server using Daphne or any other ASGI server.
+# pip install daphne
+# Run the server using daphne
+# daphne -b 0.0.0.0 -p 8000 plio.asgi:application
--- plio/settings.py
+++ plio/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import logging
 from corsheaders.defaults import default_headers
+# for load environment variables from .env file
+# from dotenv import load_dotenv
+# load_dotenv()
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
